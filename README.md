a9dfa5ccb122e2932d002ed68d0bcf249a29a122
Jimmy <sikulajakub2005@gmail.com>
2024-01-11 18:44:04 +0100

    hodne zmeny

--- README.md
+++
@@ -1,2 +0,0 @@
-# Santova aplikace
- Aplikace pro Santu
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.pepe {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.pepe to javafx.fxml;
-    exports com.example.pepe;
-}
